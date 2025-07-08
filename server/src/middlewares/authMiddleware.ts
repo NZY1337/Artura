7c0978297173b4461d56cf0323f5b5cd075d7a5f
Andrei Mocanu <mandreicosmin@yahoo.com>
2025-07-08 13:12:15 +0300

    clerk updated

--- server/src/middlewares/authMiddleware.ts
+++ server/src/middlewares/authMiddleware.ts
@@ -1,28 +1,27 @@
 import {
-  BadRequestException,
-  ErrorCode,
-  NotFoundException,
-  UnauthorizedException,
+    BadRequestException,
+    ErrorCode,
+    NotFoundException,
+    UnauthorizedException,
 } from "../middlewares/errorMiddleware";
 import { Request, Response, NextFunction } from "express";
 import { clerkMiddleware, getAuth } from "@clerk/express";
 
 export const authMiddleware = (
-  req: Request,
-  res: Response,
-  next: NextFunction
+    req: Request,
+    res: Response,
+    next: NextFunction
 ) => {
-  const auth = getAuth(req);
-  console.log(auth.userId, "from hook");
+    const auth = getAuth(req);
 
-  if (!auth.userId) {
-    throw new UnauthorizedException(
-      ErrorCode.UNAUTHORIZED,
-      "You need to login first"
-    );
-  }
+    if (!auth.userId) {
+        throw new UnauthorizedException(
+            ErrorCode.UNAUTHORIZED,
+            "You need to login first"
+        );
+    }
 
-  req.auth = auth;
+    req.auth = auth;
 
-  next();
+    next();
 };
--- server/src/webhooks/clerk/index.ts
+++ server/src/webhooks/clerk/index.ts
@@ -2,121 +2,119 @@ import { Request, Response } from "express";
 import { Webhook } from "svix";
 import { CLERK_WEBHOOK_SIGNING_SECRET } from "../../../secrets";
 import {
-  BadRequestException,
-  InternalException,
-  ErrorCode,
+    BadRequestException,
+    InternalException,
+    ErrorCode,
 } from "../../middlewares/errorMiddleware";
 import { prismaClient } from "../../services/prismaClient";
 import { Role } from "@prisma/client";
 import { clerkClient } from "@clerk/express";
 
 export const clerkWebhook = async (req: Request, res: Response) => {
-  try {
-    const wh = new Webhook(CLERK_WEBHOOK_SIGNING_SECRET);
-
-    const svixId = req.headers["svix-id"];
-    const svixTimestamp = req.headers["svix-timestamp"];
-    const svixSignature = req.headers["svix-signature"];
-
-    if (!svixId || !svixTimestamp || !svixSignature) {
-      console.error("Error: Missing Svix headers");
-      throw new BadRequestException(
-        ErrorCode.BAD_REQUEST,
-        "Missing Svix headers"
-      );
-    }
-
-    const headers = {
-      "svix-id": svixId as string,
-      "svix-timestamp": svixTimestamp as string,
-      "svix-signature": svixSignature as string,
-    };
-
-    const payload = req.body;
-    const body = JSON.stringify(payload);
-    let evt: any;
-
-    // Verify payload with headers
     try {
-      evt = wh.verify(body, headers);
+        const wh = new Webhook(CLERK_WEBHOOK_SIGNING_SECRET);
+        const svixId = req.headers["svix-id"];
+        const svixTimestamp = req.headers["svix-timestamp"];
+        const svixSignature = req.headers["svix-signature"];
+
+        if (!svixId || !svixTimestamp || !svixSignature) {
+            console.error("Error: Missing Svix headers");
+            throw new BadRequestException(
+                ErrorCode.BAD_REQUEST,
+                "Missing Svix headers"
+            );
+        }
+
+        const headers = {
+            "svix-id": svixId as string,
+            "svix-timestamp": svixTimestamp as string,
+            "svix-signature": svixSignature as string,
+        };
+
+        const payload = req.body;
+        const body = JSON.stringify(payload);
+        let evt: any;
+
+        // Verify payload with headers
+        try {
+            evt = wh.verify(body, headers);
+        } catch (err) {
+            console.error("Error: Could not verify webhook:", err);
+            throw new BadRequestException(
+                ErrorCode.BAD_REQUEST,
+                "Verification error"
+            );
+        }
+
+        try {
+            console.log("Received event:", evt.type);
+            // Handle specific event types
+            if (evt.type === "user.created") {
+                const { id, email_addresses, username, role, created_at, updated_at } = evt.data;
+                await clerkClient.users.updateUserMetadata(id, {
+                    publicMetadata: { role: "user" },
+                });
+
+                console.log("User created:", id, email_addresses[0].email_address);
+                const user = {
+                    id,
+                    email: email_addresses[0].email_address,
+                    username: username || null,
+                    role: Role.user,
+                    createdAt: new Date(created_at),
+                    updatedAt: new Date(updated_at),
+                };
+
+                await prismaClient.user.create({
+                    data: { ...user },
+                });
+            }
+
+            if (evt.type === "user.updated") {
+                const {
+                    id,
+                    email_addresses,
+                    username,
+                    role,
+                    created_at,
+                    updated_at,
+                    public_metadata,
+                } = evt.data;
+
+                const user = {
+                    id,
+                    email: email_addresses[0].email_address,
+                    username: username || null,
+                    role: public_metadata.role || Role.user,
+                    createdAt: new Date(created_at),
+                    updatedAt: new Date(updated_at),
+                };
+
+                await prismaClient.user.update({
+                    where: { id: user.id },
+                    data: { ...user },
+                });
+            }
+
+            if (evt.type === "user.deleted") {
+                const { id } = evt.data;
+                console.log("---user-deleted");
+                await prismaClient.user.delete({
+                    where: { id },
+                });
+            }
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({ error });
+        }
+
+        return res.status(200).json({ success: true });
     } catch (err) {
-      console.error("Error: Could not verify webhook:", err);
-      throw new BadRequestException(
-        ErrorCode.BAD_REQUEST,
-        "Verification error"
-      );
+        console.error("Error verifying webhook:", err);
+        // return res.status(500).json({ error: 'Internal server error' });
+        throw new InternalException(
+            ErrorCode.INTERNAL_EXCEPTION,
+            "Internal server error"
+        );
     }
-
-    try {
-      // Handle specific event types
-      if (evt.type === "user.created") {
-        const { id, email_addresses, username, role, created_at, updated_at } =
-          evt.data;
-
-        await clerkClient.users.updateUserMetadata(id, {
-          publicMetadata: { role: "user" },
-        });
-
-        const user = {
-          id,
-          email: email_addresses[0].email_address,
-          username: username || null,
-          role: Role.user,
-          createdAt: new Date(created_at),
-          updatedAt: new Date(updated_at),
-        };
-
-        await prismaClient.user.create({
-          data: { ...user },
-        });
-      }
-
-      if (evt.type === "user.updated") {
-        const {
-          id,
-          email_addresses,
-          username,
-          role,
-          created_at,
-          updated_at,
-          public_metadata,
-        } = evt.data;
-        console.log("---user-updated", evt.data);
-        const user = {
-          id,
-          email: email_addresses[0].email_address,
-          username: username || null,
-          role: public_metadata.role || Role.user,
-          createdAt: new Date(created_at),
-          updatedAt: new Date(updated_at),
-        };
-
-        console.log(user, "---user-updated");
-        await prismaClient.user.update({
-          where: { id: user.id },
-          data: { ...user },
-        });
-      }
-
-      if (evt.type === "user.deleted") {
-        const { id } = evt.data;
-        console.log("---user-deleted");
-        await prismaClient.user.delete({
-          where: { id },
-        });
-      }
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({ error });
-    }
-
-    return res.status(200).json({ success: true });
-  } catch (err) {
-    console.error("Error verifying webhook:", err);
-    // return res.status(500).json({ error: 'Internal server error' });
-    throw new InternalException(
-      ErrorCode.INTERNAL_EXCEPTION,
-      "Internal server error"
-    );
-  }
 };
